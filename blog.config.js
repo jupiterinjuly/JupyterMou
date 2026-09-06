5f6ae7ff4d2530df05d3f7a485aa017e8a1f4771
mouxinyi <mouxinyi@bytedance.com>
2026-09-06 15:47:40 +0800

    修复首页排序、日期与站点资源
    Co-Authored-By: Aime <aime@bytedance.com>
    Change-Id: I0b537533e4086bd104b5b5f74fe03c72b8ec61dd

--- blog.config.js
+++ blog.config.js
@@ -17,9 +17,9 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Jupyter Mou', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '西南财经大学 22级金融学（经管国际化创新实验班）｜深圳大学 2026级人工智能', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://jupytermou.cn', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.jupytermou.cn', // 网站正式地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'NotionNext, 博客, 牟馨怡, Jupiter, blog', // 网站关键词 英文逗号隔开
-  BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || 'https://file.notion.so/f/f/9d900ef7-c8a0-81fb-a0d1-0003964db899/9c1223cd-4199-4d22-889b-b9c66bfaf278/favicon.ico?table=block&id=2b400ef7-c8a0-80a4-b6bb-f0c4de897d05&spaceId=9d900ef7-c8a0-81fb-a0d1-0003964db899&expirationTimestamp=1763942400000&signature=LZYqXNlk5-a-bYu1cOpjEXzN3wgGkZidN7knmgMECf4&downloadName=favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
+  BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // 使用 public 中的稳定静态资源，避免 Notion 临时签名链接过期
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '蜀ICP备2025170376号', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
   BEI_AN_GONGAN: process.env.NEXT_PUBLIC_BEI_AN_GONGAN || '川公网安备51010602003010号', // 公安备案号，例如 '浙公网安备3xxxxxxxx8号'
